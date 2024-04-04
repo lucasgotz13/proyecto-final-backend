222871ebaec56191adc2ff489afe46a5f0549f36
Lucas Gotz <tutigotz@gmail.com>
2024-04-04 19:41:59 -0300

    feat: Added populate to users collection

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -3,29 +3,33 @@ import mongoose from "mongoose";
 const usersCollection = "users";
 
 const userSchema = new mongoose.Schema({
-    first_name: {
-        type: String,
-    },
-    last_name: {
-        type: String,
-    },
-    email: {
-        type: String,
-        unique: true,
-    },
-    age: {
-        type: String,
-    },
-    password: {
-        type: String,
-    },
-    cart: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "carts",
-    },
-    role: {
-        type: String,
-    },
+  first_name: {
+    type: String,
+  },
+  last_name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  age: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  cart: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "carts",
+  },
+  role: {
+    type: String,
+  },
 });
+
+userSchema.pre("findOne", function() {
+  this.populate("cart")
+})
 
 export const userModel = mongoose.model(usersCollection, userSchema);
